0d63522f63cc899a776ba44bfd1871072424d622
gaurav <gauravswargiary0702@gmail.com>
2025-12-14 13:37:48 +0530

    scope updated

--- scope.js
+++ scope.js
@@ -24,3 +24,28 @@ myFunction();
     console.log(blockVar);
     console.log(blockConst);
 }
+
+// nested scope                             child function can access the variable of parent function
+function parent() {                         // parent function
+    const username = "tony stark"
+
+    function child() {                      // child function
+        const website = "avenger"
+        console.log(username, website);     // child function is accessing the variable of parent function since parent function is globle for the child
+    }
+    // console.log(website);                // variable of child function only accessible by the child parent
+    child();                                // child function call
+}
+parent();                                   // parent functoin call 
+
+// ********************************** MINI HOISTING **********************************
+console.log(addOne(5));             // memory is created then the execution is happend
+function addOne(num1) {             // Function Declaration ✅ (Fully Hoisted)
+    return num1 + 1;
+}
+
+
+console.log(addTwo(5));             // error occures Cannot access 'addTwo' before initialization
+const addTwo = function(num2) {
+    return num2 + 2;
+}
